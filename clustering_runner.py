6b586afb1985aaf3c5610e07011a18322bba14b1
Omri <shenhav.omri@gmail.com>
2023-01-20 13:46:39 +0200

    Fixes

--- clustering_runner.py
+++ clustering_runner.py
@@ -3,25 +3,48 @@ import numpy as np
 from kmeans import kmeans
 from singlelinkage import singlelinkage
 
+K_MEAN_SAMPLE_SIZE = 100
+SINGLELINKAGE_SAMPLE_SIZE = 30
+
+
+def get_sample(sample_size):
+    data_examples = []
+    for i in range(10):
+        curr_data = data[f"train{i}"]
+        indices = np.random.choice(range(len(curr_data)), sample_size)
+        data_examples.append(curr_data[indices])
+    return np.concatenate(data_examples)
+
 
 def question_2_c():
     k = 10
+    X = get_sample(K_MEAN_SAMPLE_SIZE)
     clusters = kmeans(X, k=10, t=10)
     calculate_clustering_error("Question 2c", "kmeans", clusters, k=k, total_size=1000)
 
 
 def question_2_d():
     k = 10
+    X = get_sample(SINGLELINKAGE_SAMPLE_SIZE)
     clusters = singlelinkage(X, k=k)
+    clusters = np.unique(clusters)
+    for cluster_number, i in enumerate(clusters):
+        clusters[clusters == i] = cluster_number
     calculate_clustering_error("Question 2d", "singlelinkage", clusters, k=k, total_size=1000)
 
 
 def question_2_e():
     k = 6
+
+    X = get_sample(K_MEAN_SAMPLE_SIZE)
     clusters = kmeans(X, k=k, t=10)
     calculate_clustering_error("Question 2e - kmeans", "kmeans", clusters, k=k, total_size=1000)
 
+    X = get_sample(SINGLELINKAGE_SAMPLE_SIZE)
     clusters = singlelinkage(X, k=k)
+    clusters = np.unique(clusters)
+    for cluster_number, i in enumerate(clusters):
+        clusters[clusters == i] = cluster_number
     calculate_clustering_error("Question 2e - singlelinkage", "singlelinkage", clusters, k=k, total_size=1000)
 
 
@@ -39,16 +62,13 @@ def calculate_clustering_error(title, clustering_type, clusters, k, total_size):
 
 
 def calc_single_cluster(clusters, cluster_index):
-    indexes = np.where(clusters == cluster_index)[0]
-    labels_counts, _ = np.histogram(indexes // 100, range(11))
+    cluster_indexes = np.where(clusters == cluster_index)[0]
+    labels_counts, _ = np.histogram(cluster_indexes // 100, range(11))
     common_label = np.argmax(labels_counts)
 
     cluster_size = np.count_nonzero(clusters == cluster_index)
     labels_total_count = sum(labels_counts)
-    percentage = 0
-    if labels_total_count > 0:
-        percentage = labels_counts[common_label] / labels_total_count
-
+    percentage = labels_counts[common_label] / labels_total_count
     percentage = f"{percentage:.2f}"
 
     print(
@@ -58,12 +78,6 @@ def calc_single_cluster(clusters, cluster_index):
 
 if __name__ == '__main__':
     data = np.load('mnist_all.npz')
-    data_examples = []
-    for i in range(10):
-        curr_data = data[f"train{i}"]
-        indices = np.random.choice(range(curr_data.shape[0]), 100)
-        data_examples.append(curr_data[indices])
-    X = np.concatenate(data_examples)
 
     question_2_c()
     question_2_d()
